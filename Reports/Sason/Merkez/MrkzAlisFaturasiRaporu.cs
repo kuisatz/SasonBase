6e9410e1c692008224009b446756bb70bda69527
Oki Oki <ociran@gmail.com>
2018-04-12 11:16:29 +0300

    Alış Faturası Raporu

---
+++ Reports/Sason/Merkez/MrkzAlisFaturasiRaporu.cs
@@ -0,0 +1,135 @@
+﻿using Antibiotic.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace SasonBase.Reports.Sason.Merkez
+{
+    /// <summary>
+    /// Merkez Servis Hareket Raporu
+    /// </summary>
+    public class MrkzServisHareketRaporu : Base.SasonMerkezReporter
+    {
+        public MrkzServisHareketRaporu()
+        {
+            Text = "Servis Hareket Raporu";
+            SubjectCode = "MrkzServisHareketRaporu";
+            SubjectCode = this.getType().Name;
+            ReportFileCode = this.getType().Name;
+            AddParameter(new ReporterParameter() { Name = "param_start_date", Text = "Başlangıç Tarihi" }.CreateDate());
+            AddParameter(new ReporterParameter() { Name = "param_finish_date", Text = "Bitiş Tarihi" }.CreateDate());
+            AddParameter(new ReporterParameter() { Name = "param_servisler", Text = "Servisler" }.CreateServislerSelect(true));
+        //    AddParameter(new ReporterParameter() { Name = "param_sase_no", Text = "Şase No" }.CreateTextBox("İsteğe Bağlı Şase No. Girebilirsiniz"));
+            Disabled = false;
+        }
+        public MrkzServisHareketRaporu(decimal servisId, DateTime startDate, DateTime finishDate) : this()
+        {
+            base.ServisId = servisId;
+            this.StartDate = startDate;
+            this.FinishDate = finishDate;
+        }
+
+        public DateTime StartDate
+        {
+            get { return GetParameter("param_start_date").ReporterValue.cast<DateTime>(); }
+            set { SetParameterReporterValue("param_start_date", value.startOfDay()); }
+        }
+
+        public DateTime FinishDate
+        {
+            get { return GetParameter("param_finish_date").ReporterValue.cast<DateTime>(); }
+            set { SetParameterReporterValue("param_finish_date", value.endOfDay()); }
+        }
+
+        public List<decimal> ServisIds
+        {
+            get { return GetParameter("param_servisler").ReporterValue.cast<List<decimal>>(); }
+            set { SetParameterReporterValue("param_servisler", value); }
+        }
+   /*     public string SaseNo
+        {
+            get { return GetParameter("param_sase_no").ReporterValue.toString(); }
+            set { SetParameterReporterValue("param_sase_no", value.toString()); }
+        }
+
+            */
+        public override ReporterParameter SetParameterIncomingValue(string parameterName, object value)
+        {
+            switch (parameterName)
+            {
+                //Dışarıdan Gelen Format 20171231235959 Şeklinde Olmalıdır
+                case "param_start_date":
+                    StartDate = Convert.ToInt64(value).toDateTime();
+                    break;
+                case "param_finish_date":
+                    FinishDate = Convert.ToInt64(value).toDateTime();
+                    break;
+                case "param_servisler":
+                    ServisIds = value.toString().split(',').select(t => Convert.ToDecimal(t)).toList();
+                    break;
+        /*        case "param_sase_no":
+                    SaseNo = value.toString();
+                    break;
+                    */
+            }
+            return base.SetParameterIncomingValue(parameterName, value);
+        }
+
+        public override object ExecuteReport(MethodReturn refMr = null)
+        {
+            decimal selectedServisId = ServisIds.first().toString("0").cto<decimal>();
+            string servisIdQuery = $" = {selectedServisId}";
+            string dateQuery = "";
+
+#if DEBUG
+             selectedServisId = ServisId;
+              servisIdQuery = $" in( {selectedServisId} )";
+#endif
+
+
+            if (ServisIds.isNotEmpty())
+                servisIdQuery = $" in ({ServisIds.joinNumeric(",")}) ";
+            else { 
+            //    servisIdQuery = $" > 1 ";
+                selectedServisId = ServisId;
+                servisIdQuery = $" in( {selectedServisId} )";
+            }
+
+
+            StartDate = StartDate.startOfDay(); 
+            FinishDate = FinishDate.endOfDay();
+            dateQuery = ""+StartDate.ToString("dd/MM/yyyy") +  "' AND '"+ FinishDate.ToString("dd/MM/yyyy")+"";
+            MethodReturn mr = new MethodReturn();
+
+            List<object> queryResults = AppPool.EbaTestConnector.CreateQuery($@" 
+            SELECT 
+                DISTINCT b.servisid,
+                (select vtsx.partnercode from vt_servisler vtsx where vtsx.servisid = b.servisid  and vtsx.dilkod = 'Turkish') as partnercode,
+                (Select vtsxy.ISORTAKAD FROM vt_servisler vtsxy where  vtsxy.dilkod = 'Turkish' and vtsxy.servisid = b.servisid) as servisad,
+                a.*, 
+                b.TARIH, 
+                d.kod, 
+                d.ad
+            FROM servisstokhareketdetaylar  a 
+            INNER JOIN servisstokhareketler b on B.ID = A.SERVISSTOKHAREKETID
+            INNER JOIN servissiparisler c on c.servisid = b.servisid and c.siparisservisid=1 
+            INNER JOIN servisstoklar d on d.id = A.SERVISSTOKID 
+            WHERE 
+                a.stokislemtipdeger=1 and 
+                b.servisid {servisIdQuery}  AND 
+                b.TARIH between '{dateQuery}' 
+            ORDER BY b.servisid , b.TARIH desc 
+                ")
+              .GetDataTable(mr)
+               .ToModels();
+             
+            CloseCustomAppPool();
+            return queryResults;
+        }
+
+    }
+}
---
+++ Reports/Sason/Servis/SrvsAlisFaturasiRaporu.cs
@@ -0,0 +1,135 @@
+﻿using Antibiotic.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace SasonBase.Reports.Sason.Servis
+{
+    /// <summary>
+    ///  Alış Faturası Rapor
+    /// </summary>
+    public class SrvsAlisFaturasiRaporu : Base.SasonReporter
+    {
+        public SrvsAlisFaturasiRaporu()
+        {
+            Text = "Alış Faturası Raporu";
+            SubjectCode = "SrvsAlisFaturasiRaporu";
+            SubjectCode = this.getType().Name;
+            ReportFileCode = this.getType().Name;
+            AddParameter(new ReporterParameter() { Name = "param_start_date", Text = "Başlangıç Tarihi" }.CreateDate());
+            AddParameter(new ReporterParameter() { Name = "param_finish_date", Text = "Bitiş Tarihi" }.CreateDate());
+       //     AddParameter(new ReporterParameter() { Name = "param_servisler", Text = "Servisler" }.CreateServislerSelect(true));
+        //    AddParameter(new ReporterParameter() { Name = "param_sase_no", Text = "Şase No" }.CreateTextBox("İsteğe Bağlı Şase No. Girebilirsiniz"));
+            Disabled = false;
+        }
+        public SrvsAlisFaturasiRaporu(decimal servisId, DateTime startDate, DateTime finishDate) : this()
+        {
+            base.ServisId = servisId;
+            this.StartDate = startDate;
+            this.FinishDate = finishDate;
+        }
+
+        public DateTime StartDate
+        {
+            get { return GetParameter("param_start_date").ReporterValue.cast<DateTime>(); }
+            set { SetParameterReporterValue("param_start_date", value.startOfDay()); }
+        }
+
+        public DateTime FinishDate
+        {
+            get { return GetParameter("param_finish_date").ReporterValue.cast<DateTime>(); }
+            set { SetParameterReporterValue("param_finish_date", value.endOfDay()); }
+        }
+
+        public List<decimal> ServisIds
+        {
+            get { return GetParameter("param_servisler").ReporterValue.cast<List<decimal>>(); }
+            set { SetParameterReporterValue("param_servisler", value); }
+        }
+   /*     public string SaseNo
+        {
+            get { return GetParameter("param_sase_no").ReporterValue.toString(); }
+            set { SetParameterReporterValue("param_sase_no", value.toString()); }
+        }
+
+            */
+        public override ReporterParameter SetParameterIncomingValue(string parameterName, object value)
+        {
+            switch (parameterName)
+            {
+                //Dışarıdan Gelen Format 20171231235959 Şeklinde Olmalıdır
+                case "param_start_date":
+                    StartDate = Convert.ToInt64(value).toDateTime();
+                    break;
+                case "param_finish_date":
+                    FinishDate = Convert.ToInt64(value).toDateTime();
+                    break;           
+        /*        case "param_sase_no":
+                    SaseNo = value.toString();
+                    break;
+                    */
+            }
+            return base.SetParameterIncomingValue(parameterName, value);
+        }
+
+        public override object ExecuteReport(MethodReturn refMr = null)
+        {
+            decimal selectedServisId = ServisIds.first().toString("0").cto<decimal>();
+            string servisIdQuery = $" = {selectedServisId}";
+            string dateQuery = "";
+
+#if DEBUG
+             selectedServisId = ServisId;
+              servisIdQuery = $" in( {selectedServisId} )";
+#endif
+
+
+            if (ServisIds.isNotEmpty())
+                servisIdQuery = $" in ({ServisIds.joinNumeric(",")}) ";
+            else { 
+            //    servisIdQuery = $" > 1 ";
+                selectedServisId = ServisId;
+                servisIdQuery = $" in( {selectedServisId} )";
+            }
+
+
+            StartDate = StartDate.startOfDay(); 
+            FinishDate = FinishDate.endOfDay();
+            dateQuery = ""+StartDate.ToString("dd/MM/yyyy") +  "' AND '"+ FinishDate.ToString("dd/MM/yyyy")+"";
+            MethodReturn mr = new MethodReturn();
+
+            List<object> queryResults = AppPool.EbaTestConnector.CreateQuery($@" 
+            select 
+                    (select vtsx.partnercode from vt_servisler vtsx where vtsx.servisid = a.servisid  and vtsx.dilkod = 'Turkish') as partnercode,
+                    (Select vtsxy.ISORTAKAD FROM vt_servisler vtsxy where  vtsxy.dilkod = 'Turkish' and vtsxy.servisid = a.servisid) as servisad,
+                    a.faturaturid, 
+                    a.faturano, 
+                    a.islemtarihi,
+                    a.cariunvan,
+                    a.bruttoplam, 
+                    a.indirimtoplam,
+                    a.netkdvtoplam, 
+                    a.nettutar, 
+                    a.toplam,
+                    a.carikod, 
+                    a.vno,
+                    a.servisid
+                FROM faturalar a
+                WHERE a.servisid  {servisIdQuery} 
+                and a.islemtarihi between '{dateQuery}'
+                and a.faturaturid=4 
+            ORDER BY servisad ,a.islemtarihi desc 
+                ")
+              .GetDataTable(mr)
+               .ToModels();
+             
+            CloseCustomAppPool();
+            return queryResults;
+        }
+
+    }
+}
