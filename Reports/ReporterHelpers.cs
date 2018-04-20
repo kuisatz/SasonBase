cc049017fa9d3463ea0743d987e653a9402780dd
Oki Oki <ociran@gmail.com>
2018-04-20 12:32:07 +0300

    Agrega Katalog - eklenen yeni raporlardan birisi

--- Reports/ReporterHelpers.cs
+++ Reports/ReporterHelpers.cs
@@ -370,6 +370,68 @@ namespace SasonBase.Reports
             return param.ReplaceParamTextName();
         }
 
+        public static ReporterParameter CreateAgregaSelect(this ReporterParameter param, bool multipleSelect)
+        {
+            string selectType = (multipleSelect ? "multiple" : "single");
+
+            param.Html = $@"<div class='option'><span>parameter_text</span><div id='parameter_name'></div></div>";
+            param.Js = @"
+            <script>$(function() {
+
+                var dataGrid = $('#parameter_name').dxDataGrid({
+                    selection:
+                        {
+                            mode: '" + selectType + @"'
+                    },
+                    paging:
+                        {
+                            pageSize: 12
+                    },
+                    filterRow:
+                        {
+                            visible: true
+                    },
+                    columns: [
+                        {
+                            dataField: 'ID', 
+                            caption: 'ID',
+                            width: 90,
+                            visible: false
+                        },
+                        {
+                            dataField:'KOD',
+                            caption:'Recete',
+                            width:400
+                        }
+                        ],
+                    onSelectionChanged: function (selectedItems) {
+                                var receteIds = '';
+                                $.each(selectedItems.selectedRowKeys, function(index, item) {
+                                    receteIds += item.ID + ','; 
+                                });
+                                jQuery.data(document.body, 'parameter_name_value', receteIds);
+                        }
+                }).dxDataGrid('instance');
+
+                jQuery.data(document.body, 'parameter_name_value', '');        
+
+                dataGrid.option('selection.selectAllMode', 'allPages');
+                dataGrid.option('selection.showCheckBoxesMode', 'onClick');
+
+                action('Sason', 'GetAgregaListesi', null, function (res) {
+                    if (res.Ok) {
+                       getGrid('parameter_name').option('dataSource', res.Data);
+                    }
+                }
+                );
+ 
+            }
+            )   
+            </script>
+            ";
+            return param.ReplaceParamTextName();
+        }
+
         public static ReporterParameter CreateIsEmirHizmetYeri(this ReporterParameter param, bool multipleSelect)
         {
             string selectType = (multipleSelect ? "multiple" : "single");
---
+++ Reports/Sason/Servis/SrvsAgregaKatalog.cs
@@ -0,0 +1,93 @@
+﻿using Antibiotic.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace SasonBase.Reports.Sason.Servis
+{
+    /// <summary>
+    ///  Agrega Kataloğu
+    /// </summary>
+    public class SrvsAgregaKatalog : Base.SasonReporter
+    {
+        public SrvsAgregaKatalog()
+        {
+            Text = "Agrega Kataloğu";
+            SubjectCode = "SrvsAgregaKatalog";
+            SubjectCode = this.getType().Name;
+            ReportFileCode = this.getType().Name;
+            AddParameter(new ReporterParameter() { Name = "param_agreta", Text = "Agrega" }.CreateAgregaSelect(true));
+            Disabled = false;
+        }
+        public SrvsAgregaKatalog(decimal servisId, decimal agretaId, DateTime startDate, DateTime finishDate) : this()
+        {
+            base.ServisId = servisId;
+          //  this.StartDate = startDate;
+         //   this.FinishDate = finishDate;
+        }
+
+        public List<decimal> AgretaIds
+        {
+            get { return GetParameter("param_agreta").ReporterValue.cast<List<decimal>>(); }
+            set { SetParameterReporterValue("param_agreta", value); }
+        }
+
+        public override ReporterParameter SetParameterIncomingValue(string parameterName, object value)
+        {
+            switch (parameterName)
+            {
+                case "param_agreta":
+                    AgretaIds = value.toString().split(',').select(t => Convert.ToDecimal(t)).toList();
+                    break;
+
+            }
+            return base.SetParameterIncomingValue(parameterName, value);
+        }
+
+        public override object ExecuteReport(MethodReturn refMr = null)
+        {
+            //  string dateQuery = "";
+            decimal selectedAgretaId = AgretaIds.first().toString("0").cto<decimal>();
+            string agretaIdQuery = $" = {selectedAgretaId}";
+
+            if (AgretaIds.isNotEmpty())
+                agretaIdQuery = $" in ({AgretaIds.joinNumeric(",")}) ";
+            else
+            {
+                selectedAgretaId = 4;
+                agretaIdQuery = $" in( {selectedAgretaId} )";
+            }
+
+            MethodReturn mr = new MethodReturn();
+
+            List<object> queryResults = AppPool.EbaTestConnector.CreateQuery($@" 
+                    SELECT 
+                        AGR.AD AGREGA_ADI, 
+                        Q.AD AGREGA_OZELLIK, 
+                        T.AD AGREGA_TIPI, 
+                        TIP.DEGER KULLANILACAK_DEGER 
+                    FROM  VW_AGREGATIPLER T  
+                    LEFT JOIN vw_agregalar agr ON T.AGREGAID = AGR.ID 
+                    LEFT JOIN AGREGATIPOZELLIKLER tip ON TIP.AGREGATIPID= t.id  
+                    LEFT JOIN vw_AGREGAOZELLIKLER q ON Q.ID= TIP.AGREGAOZELLIKID 
+                    WHERE   
+                        agr.dilkod='Turkish' and 
+                        Q.DILKOD = 'Turkish' and 
+                        t.dilkod = 'Turkish' AND 
+                        AGR.ID  {agretaIdQuery} 
+                    ORDER BY AGR.AD ,Q.AD , T.AD 
+       
+                ")
+              .GetDataTable(mr)
+               .ToModels();
+
+            CloseCustomAppPool();
+            return queryResults;
+        }
+
+    }
+}
