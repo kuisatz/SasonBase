07ee9ab4ff90830498125961c9476db97a0fa048
Oki Oki <ociran@gmail.com>
2018-05-09 14:10:14 +0300

    garanti red raporu sql  değişikliği

--- Reports/Sason/Servis/SrvsGarantiRedRaporu.cs
+++ Reports/Sason/Servis/SrvsGarantiRedRaporu.cs
@@ -68,68 +68,57 @@ namespace SasonBase.Reports.Sason.Servis
 
 
             List<object> queryResults = AppPool.EbaTestConnector.CreateQuery($@"
-                    SELECT -- FLOWDOCUMENTS.FILEPROFILEID,
-                            --   FLOWDOCUMENTS.PROCESSID,        
-                             (select vtsx.partnercode from vt_servisler vtsx where vtsx.servisid = e_ssi31_form.SERVISID and vtsx.dilkod = 'Turkish') as partnercode,
-                             (Select vtsxy.ISORTAKAD FROM vt_servisler vtsxy where  vtsxy.dilkod = 'Turkish' and vtsxy.servisid = e_ssi31_form.SERVISID )  as servisad,          
-                             OSUSERS.FIRSTNAME || ' ' || OSUSERS.LASTNAME AS AkisiBaslatan,
-                             LIVEFLOWS.RDATE,
-                             LIVEFLOWS.FDATE,
-                             FLOWSTATUSES.DESCRIPTION,
-                             FLOWSTATUSES.STATUS,         
-                             e_ssi31_form.FIRMAAD,
-                             e_ssi31_form.garantituru,
-                             e_ssi31_form.garantistatu,
-                             e_ssi31_form.garantirednedenid_text,
-                             (SELECT esagarantino
-                                FROM ayristirmalar g
-                               WHERE g.id = e_ssi31_form.ayristirmaid)  ESAGARANTINO,
-                            (select C.KOD from bakimsozlesmeler a, araclar b, bakimsozlesmetipler c where a.aracid = b.id and c.id = A.BAKIMSOZLESMETIPID and saseno = e_ssi31_form.saseno and a.durumid = 1) bakimsozlesmetipi,
-                                 e_ssi31_form.REGNUMBER PLAKA, (SELECT esastatu  FROM ayristirmalar g WHERE g.id = e_ssi31_form.ayristirmaid)  ESASTATU,
-                             (SELECT t.ad
-                                FROM vw_ayristirmatipler t, ayristirmalar a
-                               WHERE     a.ayristirmatipid = t.id
-                                     AND a.id = e_ssi31_form.ayristirmaid
-                                     AND dilkod = 'Turkish')
-                                AYRISTIRMATIPAD,
-                             e_ssi31_form.servisgarantino,
-                             e_ssi31_form.saseno,
-                             e_ssi31_form.SERVISID,
-                             e_ssi31_form.isemirno,
-                          --e_ssi31_form.ISLACIKLAMA,
-                             e_ssi31_form.islemarizakod,
-                             e_ssi31_form.islemtipad,
-                             e_ssi31_form.regnumber,
-                        --e_ssi31_form.islemtipad,
-                             e_ssi31_form.firstregdate,
-                             e_ssi31_form.exworksdate,
-                             e_ssi31_form.vehiclekm,
-                             e_ssi31_form.modelnum,
-                             e_ssi31_form.engineserialnum,
-                             e_ssi31_form.engineunittype,
-                             e_ssi31_form.pkm,
-                             e_ssi31_form.uom,
-                             NVL(e_ssi31_form.belgeno, '-') belgeno
-                       FROM FLOWDOCUMENTS
-                            INNER JOIN LIVEFLOWS ON FLOWDOCUMENTS.PROCESSID = LIVEFLOWS.ID
-                             INNER JOIN e_ssi31_form
-                                ON FLOWDOCUMENTS.FILEPROFILEID = e_ssi31_form.ID
-                             INNER JOIN
-                             FLOWSTATUSES
-                                ON     LIVEFLOWS.STATUS = FLOWSTATUSES.STATUS
-                                   AND LIVEFLOWS.PROCESS = FLOWSTATUSES.PROCESS
-                                   AND LIVEFLOWS.FLOWVERSION = FLOWSTATUSES.VERSION
-                             INNER JOIN OSUSERS ON LIVEFLOWS.USERID = OSUSERS.ID
-                       WHERE LIVEFLOWS.DELETED = 0 AND
-                             (SELECT COUNT(*)
-                                    FROM FLOWREQUESTS
-                                   WHERE PROCESSID = LIVEFLOWS.ID) > 1 AND
-                             LIVEFLOWS.DELETED = 0 AND
-                             LIVEFLOWS.STATUS > 1 AND
-                             LIVEFLOWS.STATUS  in (22) AND 
-                            e_ssi31_form.SERVISID {servisIdQuery}  AND 
-                            LIVEFLOWS.RDATE BETWEEN'{dateQuery}'
-                     ORDER BY LIVEFLOWS.RDATE DESC 
+                    SELECT
+                        A.SERVISGARANTINO,
+                        j.partnercode  serviskodu,
+                        A.PDFgarantiTARIHi garantitarihi,
+                        A.PDFDMSNO dmsno,
+                        pdftoplam gidentutar,
+                        A.PDFONAYNETGENELTOPLAM gelentutar,
+                        A.claimstatus statu,
+                        j.isortakad yetkiliservis,
+                        a.arizakodu,
+                        a.CASENO,
+                        a.CLAIMNO,
+                        C.AD ayristirmatipi,
+                        E.AD isemirislemtipi,
+                        F.MUSTERIAD,
+                        F.KAYITTARIH isemirbastarihi,
+                        F.TAMAMLANMATARIH isemirbittarih, 
+                        f.SASENO,
+                        F.ISEMIRNO,
+                        G.AD musteri,
+                        h.regnumber plaka,
+                        h.ad Turu
+                    FROM ayristirmalar a,vw_ayristirmatipler c, servisisemirislemler d,vw_isemirtipler e,servisisemirler f,servisvarliklar g,
+                      (SELECT a.saseno,
+                        m.regnumber,
+                        m.vehicletype,
+                        b.ad,
+                        dilkod
+                    FROM araclar a,
+                        esaaraclar ea,
+                        vx_vis_vehiclemaster m,vw_aracturler b
+                    WHERE     ea.vin = a.saseno
+                        AND ea.id = m.esaaracid
+                        and M.VEHICLETYPE=b.kod) h, vt_servisler j
+                    where
+                    a.AYRISTIRMATIPID=c.id
+                    and c.dilkod='Turkish'
+                    and a.SERVISISEMIRISLEMID=D.ID
+                    and D.ISEMIRTIPID=E.ID
+                    and e.dilkod=c.dilkod
+                    and D.servisisemirid=f.id
+                    and F.SERVISVARLIKID=G.ID
+                    and h.saseno=f.saseno
+                    and h.dilkod=c.dilkod
+                    and a.servisid=j.servisid
+                    and j.dilkod=e.dilkod
+                    and a.tarih between '{dateQuery}'
+                    and a.servisid  {servisIdQuery} 
+                    and a.claimstatus in ('Z107','Z109','Z999') 
+             ORDER BY a.tarih DESC 
+                      
  
             
             ")
